12f104778382c3ff97a45d524fc9a1039bb48026
Rémi Durieu <remi.durieu@univ-rouen.fr>
2020-03-25 19:59:19 +0100

    Implements waitForInput() in Game

--- galamadriabuyak/Game.java
+++ galamadriabuyak/Game.java
@@ -1,11 +1,12 @@
-. 
+package galamadriabuyak;
 
 import galamadriabuyak.util.*;
 
 public class Game {
     
     public Game() {
-        ICombatParser Combatparser = new CombatParser();
+        Scanner scanner = new Scanner(System.in);
+        ICombatParser combatParser = new CombatParser();
         IPlayer player = new Player();
     }
     
@@ -17,29 +18,34 @@ public class Game {
         //enemy.getDeck().shuffleDeck();
         while (!player.isDead() && !enemy.isDead()) {
             player.draw(3 - player.getHand().getSize());
-            while(combatParser.getLastCommand() != combatParser.CMD_ENDTURN && !player.isDead()) {
+            while (combatParser.getLastCommand() != combatParser.CMD_ENDTURN
+            && !player.isDead()) {
                 waitForInput();
-                while(!combatParser.isLastCommandLegal()){
+                while (!combatParser.isLastCommandLegal()) {
                     waitForInput();
                 }
-                if(combatParser.getLastCommand() == CMD_USE){
+                if (combatParser.getLastCommand() == CMD_USE) {
                     player.GetHand().getCard().applyEffects(this);
                 }
-                if(combatParser.getLastCommand() == CMD_HELP){
+                if (combatParser.getLastCommand() == CMD_HELP) {
                     player.GetHand().getCard().getDescription();
                     player.GetHand().getCard().getTrivia();
                 }
             }
             enemy.draw(3 - player.getHand().getSize());
-            enemy.performMonsterTurn();
+            enemy.performTurn();
         }
+    }
+    
+    private String waitForInput() {
+        StringBuffer result = new StringBuffer();
+        while (scanner.hasNext()) {
+            result.append(scanner.next());
+        }
+        return result.toString();
     }
     
     public static void main(String[] args) {
         new Game();
-    }
-    
-    private String waitForInput(){
-        
     }
 }
