4035d9dcc562812d1d97f574c466b5f4a7d66308
MrAlienTech <kacixd76@gmail.com>
2020-03-25 14:11:29 +0100

    Update IHand.java

--- galamadriabuyak/IHand.java
+++ galamadriabuyak/IHand.java
@@ -1,18 +1,33 @@
 package galamadriabuyak;
 
-
+/**
+ * Models a player's hand. 
+ * 
+ * @pre 
+ *      0 >= getSize() >= MAX_HAND
+ */
 public interface IHand
 {
+    // Constants
     
-    //Requests
+    int MAX_HAND = 3;
+    
+    // Requests
     
     /**
      * Returns the size of the hand.
+     * Without counting the basic attack.
      */
     int getSize();
     
     /**
      * Returns the nth card of the hand.
+     * n = 0 is the basic attack.
+     * 
+     * @pre 
+     *      0 >= n >= getSize()
+     * @post
+     *      getSize() = old getSize()
      */
     ICard getCard(int n);
     
