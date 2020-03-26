d59f64d3afab738b3f69e5e43222fc0b43e5ac52
tef76 <40719576+tef76@users.noreply.github.com>
2020-03-26 18:34:06 +0100

    Implementation of Hand

--- galamadriabuyak/Hand.java
+++ galamadriabuyak/Hand.java
@@ -3,4 +3,29 @@ package galamadriabuyak;
 
 public class Hand implements IHand {
 
+    // ATTRIBUTS    
+    
+    ICard[] handTab = new ICard[MAX_SIZE];
+    
+    // REQUETES
+    
+    
+    public int getSize(){
+        return handTab.length;
+    }
+    
+    /**
+     * Returns the nth card of the hand.
+     * 
+     * @pre 
+     *      0 <= n <= getSize() - 1
+     * @post
+     *      getSize() = old getSize()
+     */
+    public ICard getCard(int n){
+        if (0 > n || getSize() - 1 > n){
+            throw new AssertionError();
+        }
+        return handTab[n];
+    }
 }
