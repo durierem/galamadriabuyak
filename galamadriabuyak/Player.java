7f48a5362769b4bcc70d403665eee1bd25b18326
MrAlienTech <kacixd76@gmail.com>
2020-03-26 16:45:23 +0100

    Updating IPlayer and Player
    Adding new attributes and getters.
    Work not finnished.

--- galamadriabuyak/Player.java
+++ galamadriabuyak/Player.java
@@ -1,7 +1,34 @@
 package galamadriabuyak;
 
 public class Player extends Character implements IPlayer {
-    public Player() {
+    // Attributes
+    
+    private final String name;
+    private int level;
+    private int money;
+    
+    // Constructor
+    
+    public Player(String name, int level, int money) {
         super();
+        
+        if (name == null || level < 0 ||money < 0) {
+            throw new AssertionError();
+        }
+        this.name = name;
+        this.level = level;
+        this.money = money;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getLevel() {
+        return level;
+    }
+    
+    public int getMoney() {
+        return money;
     }
 }
