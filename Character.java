72667cfc2f48910e5b72e62eddf7f47985970cd6
tef76 <40719576+tef76@users.noreply.github.com>
2020-03-28 17:35:36 +0100

    Debug draw on Character and addCard on Hand

---
+++ Character.java
@@ -0,0 +1,88 @@
+package galamadriabuyak;
+
+public abstract class Character implements ICharacter {
+    
+    // ATTRIBUTS
+
+    private final String name;
+    private int level;
+    private int health;
+    private ICard basicAttack;
+    private IDeck deck;
+    private IHand hand;    
+
+    // CONSTRUCTEUR
+
+    public Character(String name, int level, int health, ICard basicAttack,
+        IDeck deck, IHand hand) {
+        this.name = name;
+        this.level = level;
+        this.health = health;
+        this.basicAttack = basicAttack;
+        this.deck = deck;
+        this.hand = hand;
+    }
+
+    // REQUÊTES
+    
+    public String getName() {
+        return name;
+    }
+    
+    public int getLevel() {
+        return level;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public IDeck getDeck() {
+        return deck;
+    }
+
+    public IHand getHand(){
+        return hand;    
+    }
+    
+    public boolean isDead() {
+        return getHealth() <= 0;
+    }
+    
+    public ICard getBasicAttack(){
+        return basicAttack;
+    }
+
+    // COMMANDES
+    
+    public void setHealthTo(int q) {
+        if (q < 0) {
+          throw new AssertionError();
+        }
+        health = q;
+    }
+
+    public void setHealthUp(int q) {
+        if (q < 0 ) {
+            throw new AssertionError();
+        }
+        health += q;
+    }
+
+    public void setHealthDown(int q) {
+        if (q < 0) {
+          throw new AssertionError();
+        }
+        health -= q;
+    }
+    
+    public void draw(int n){
+        if (n < 1 || n > getDeck().getSize() 
+            || n > getHand().MAX_SIZE - getHand().getSize()){
+            throw new AssertionError();
+        }
+        for(int i = 0; i < n; i++){
+        getDeck().drawCard(getHand());
+        }
+    }
+}
---
+++ Hand.java
@@ -0,0 +1,65 @@
+package galamadriabuyak;
+
+public class Hand implements IHand {
+
+    // ATTRIBUTS    
+    
+    private ICard[] handTab = new ICard[MAX_SIZE];
+    private int size;
+    
+    // CONSTRUCTEUR
+    
+    public Hand() {
+        size = 0;
+    }
+    
+    // REQUETES
+    
+    public int getSize() {
+        return size;
+    }
+    
+    /**
+     * Returns the nth card of the hand.
+     * 
+     * @pre 
+     *      0 <= n <= getSize() - 1
+     * @post
+     *      getSize() = old getSize()
+     */
+    public ICard getCard(int n) {
+        if (0 > n || getSize() - 1 > n){
+            throw new AssertionError();
+        }
+        return handTab[n];
+    }
+    
+    // COMMANDES
+   
+    public void setSize(int n) {
+        if (0 > n || MAX_SIZE > n){
+            throw new AssertionError();
+        }
+        size = n;
+    }
+    
+    public void deleteCard(int n) {
+        if (n < 0 || getSize() - 1 > n){
+            throw new AssertionError();
+        }
+        for(int i = n; i < MAX_SIZE; i++){
+            handTab[i] = handTab[i+1];
+        }
+        handTab[getSize()] = null;
+        size = getSize() - 1;
+    }
+    
+    public void addCard(ICard card) {
+        if (size >= MAX_SIZE || card == null) {
+            throw new AssertionError();
+        }
+        
+        handTab[getSize()] = card;
+        size += 1;
+    }
+}
