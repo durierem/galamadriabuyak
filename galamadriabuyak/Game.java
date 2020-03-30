b90288eea87497992e182107ec8452ff180a1e97
Rémi Durieu <remi.durieu@univ-rouen.fr>
2020-03-30 14:06:00 +0200

    Useless code beautifying

--- galamadriabuyak/Game.java
+++ galamadriabuyak/Game.java
@@ -154,12 +154,13 @@ public class Game {
     private void clear() {
         try {
             if (System.getProperty("os.name").contains("Windows")) {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            } 
-            else {
+                new ProcessBuilder("cmd", "/c", "cls")
+                    .inheritIO().start().waitFor();
+            } else {
                Runtime.getRuntime().exec("clear");
             }
-        } catch (IOException | InterruptedException ex) {
+        } catch (IOException | InterruptedException e) {
+            // ...
         }
     }
     
