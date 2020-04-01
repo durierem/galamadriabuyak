9a1afff75023d5cacc59f51b98b40aceb983592f
MrAlienTech <kacixd76@gmail.com>
2020-04-01 14:03:01 +0200

    Create EnemyFactory.java
    + some ideas in comment

---
+++ galamadriabuyak/util/EnemyFactory.java
@@ -0,0 +1,46 @@
+package galamadriabuyak.util;
+
+import galamadriabuyak.IPlayer;
+import galamadriabuyak.IEnemy;
+import galamadriabuyak.Enemy;
+import galamadriabuyak.IDeck;
+import galamadriabuyak.Deck;
+import galamadriabuyak.IHand;
+import galamadriabuyak.Hand;
+import galamadriabuyak.BasicAttack;
+
+/**
+ * None instantiable class made to make new Enemies 
+ * with the same strenght than the player.
+ */
+public class EnemyFactory {
+    
+    private EnemyFactory() {
+    }
+    
+    public static IEnemy createEnemy(IPlayer p) {
+        String name = "Jesus"; //Still to do
+        
+        int level = (int) (p.getLevel() * alea(60, 90) / 100); //Still to adjust
+        int health = p.getHealth() * alea(60, 90) / 100; //Still to adjust
+        //Should we base health on the level, wich will be based on the player level.
+        //Because like this the level have no interest.
+        
+        BasicAttack basicAttack = null; //Still to do
+        //Choose a random basicAttack.
+        
+        IDeck deck = new Deck();
+        //We will have to fill the deck with random (or not) cards.
+        
+        IHand hand = new Hand();
+        
+        return new Enemy(name, level, health, basicAttack, deck, hand);
+    }
+    
+    /**
+     * Return a random number in range of [min, max] 
+     */
+    private static int alea(int min,int max) {
+        return min + (int) (Math.random() * (max - min + 1));
+    } 
+}
